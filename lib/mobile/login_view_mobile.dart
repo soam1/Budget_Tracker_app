d53ae298a2a2bc42aa42eced25bf09239d031539
soam1 <soamakash9@gmail.com>
2023-07-10 16:42:56 +0530

    initial commit with just creating required dart files and adding dependencies

---
+++ lib/mobile/login_view_mobile.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class LoginViewMobile extends HookConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SafeArea(
+      child: Scaffold(),
+    );
+  }
+}
---
+++ lib/view_model.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logger/logger.dart';
+
+final viewModel =
+    ChangeNotifierProvider.autoDispose<ViewModel>((ref) => ViewModel());
+
+class ViewModel extends ChangeNotifier {
+  final _auth = FirebaseAuth.instance;
+  CollectionReference userCollection =
+      FirebaseFirestore.instance.collection('users');
+  bool isSignedIn = false;
+  bool isObscure = true;
+  var logger = Logger();
+
+  List expensesName = [];
+  List expensesAmount = [];
+  List incomesName = [];
+  List incomesAmount = [];
+
+  //Check if Signed In
+  Future<void> isLoggedIn() async {
+    await _auth.authStateChanges().listen((User? user) {
+      if (user == null) {
+        isSignedIn = false;
+      } else {
+        isSignedIn = true;
+      }
+    });
+    notifyListeners();
+  }
+}
